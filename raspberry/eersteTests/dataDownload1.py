1835735ff2563029fc4dc0fcd5af57b17be16f8d
AJLaptop <arendjan18@gmail.com>
2016-08-31 11:15:52 +0200

    asfdasdfasdf

--- raspberry/eersteTests/dataDownload1.py
+++ raspberry/eersteTests/dataDownload1.py
@@ -1,15 +1,15 @@
 # _*_ coding:utf-8 _*_
 import requests
 import sys
-from BeautifulSoup import BeautifulSoup
-#import os
+#from BeautifulSoup import BeautifulSoup
+import os
 
 
 class Item(object):
     titel = ""
     prijs = ""
     omschrijving = ""
-    bereidingsmethode = ""
+
     bewaren = ""
     ingredienten = ""
     fabrikant = ""
@@ -18,7 +18,7 @@ class Item(object):
         self.titel= titel
         self.prijs = prijs
         self.omschrijving = omschrijving
-        self.bereidingsmethode = bereidingsmethode
+
         self.bewaren = bewaren
         self.ingredienten = ingredienten
         self.fabrikant = fabrikant
@@ -31,7 +31,7 @@ class Item(object):
             return description[offset:]
 
         except:
-            return "ebola"
+            return ""
 
     def bestaatItem(self, html):
         if(self.getItem(html, "name=\"description\"", "\" />", 10)=="Jumbo Groceries"):
@@ -45,7 +45,7 @@ class Item(object):
         prijstext = self.getItem(html, "<div class=\"jum-sale-price\" data-jum-role=\"price\" data-dynamic-block-name=\"SalePrice\" data-dynamic-block-id=\"\"><span class=\"jum-price-format\">", "</span>")
         self.prijs = prijstext[:prijstext.index("<sup>")]+","+prijstext[prijstext.index("<sup>")+len("<sup>"):prijstext.index("</sup>")]
         self.omschrijving = self.getItem(html, "<div class=\"jum-summary-description\">", "</p>", 3).replace("<br/>","")
-        self.bereidingsmethode = self.getItem(html, "<h3>Bereidingsmethode</h3>", "</p>",4)
+
         self.bewaren = self.getItem(html, "<div class=\"jum-product-storage jum-product-info-item\">\n<h3>Bewaren</h3>\n<p>", "</p>",0)
         self.ingredienten = self.getItem(html, "<div class=\"jum-ingredients-info jum-product-info-item\"><h3>Ingredi&euml;nten</h3><ul>","</ul>",0).replace("<li>", "x").replace("</li>", "\n")
         self.fabrikant = self.getItem(html, "<div class=\"jum-product-brand-info jum-product-info-item\"><h3>Over de fabrikant</h3><p>", "<h3>",0)
@@ -70,14 +70,19 @@ item.sloopHTML(html)
 print item.prijs
 print item.titel
 print item.omschrijving
-print item.bereidingsmethode
+
 print item.bewaren
 print item.ingredienten
 print item.fabrikant
 
-cmd = 'sudo echo \"Je hebt '+item.titel+' gescand!!!\n\n\n\n\n\" > /dev/usb/lp0'
+cmd = 'sudo echo \"Je hebt '+item.titel+' gescand!!!\nDe prijs is $'+item.prijs+'.\nDe omschrijving:'+item.omschrijving+'\nDe bereidingsmethode is:\n\n\" > /dev/usb/lp0'
+
+os.system(cmd)
+
+	
 
 
 
 
-#os.system(cmd)
+
+
