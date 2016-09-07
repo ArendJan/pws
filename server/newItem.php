b86114ab4a676c334d432eb6912e01532ceca669
VincentSLap <arendjan18@gmail.com>
2016-09-07 10:51:16 +0200

    newItem.php gitHub Fix

--- server/newItem.php
+++ server/newItem.php
@@ -3,6 +3,7 @@ require_once("php/start.php");
 $conn = db();
 
   $code = $_GET['code'];
+  echo "Barcode: $code";
 
       //Check of product al bestaat
       $checkstmt = $conn->prepare("SELECT barcode FROM producten WHERE barcode = :barcode");
@@ -13,14 +14,13 @@ $conn = db();
           echo "Bestaat!";
           //Doe +1 bij aantal van product
           $upstmt = "UPDATE producten SET aantal = aantal + 1 WHERE barcode = ?";
-          $z = $db->prepare($upstmt);
+          $z = $conn->prepare($upstmt);
           $z->execute(array($code));
       } else {
           echo "Bestaat niet!";
           //Voeg product toe
           try {
-            $addstmt = $conn->prepare("INSERT INTO producten (barcode)
-            VALUES (?)");
+            $addstmt = $conn->prepare("INSERT INTO producten (barcode, aantal) VALUES (?,1)");
             $addstmt->execute(array($code));
             }
         catch(PDOException $e)
