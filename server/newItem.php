9e648dbfca6738d0b032a27ae6375c767e94afdc
VincentSLap <arendjan18@gmail.com>
2016-09-07 15:04:20 +0200

    Minor Fix

--- server/newItem.php
+++ server/newItem.php
@@ -4,7 +4,9 @@ require_once("php/getItem.php");
 $conn = db();
 
 $code = $_GET['code'];
-echo "Barcode: $code";
+echo "Barcode: $code ";
+$desc = getTags($code);
+echo "Omschrijving: $desc ";
 
 //Check of product al bestaat
 $checkstmt = $conn->prepare("SELECT barcode FROM producten WHERE barcode = :barcode");
@@ -12,17 +14,17 @@ $checkstmt->bindParam(':barcode', $code);
 $checkstmt->execute();
 
 if($checkstmt->rowCount() > 0){
-  echo "Bestaat!";
+  echo "Bestaat! ";
   //Doe +1 bij aantal van product
   $upstmt = "UPDATE producten SET aantal = aantal + 1 WHERE barcode = ?";
   $z = $conn->prepare($upstmt);
   $z->execute(array($code));
 } else {
-  echo "Bestaat niet!";
+  echo "Bestaat niet! ";
   //Voeg product toe
   try {
     $addstmt = $conn->prepare("INSERT INTO producten (barcode, aantal,omschrijving) VALUES (?,1,?)");
-    $addstmt->execute(array($code,getTags($code)));
+    $addstmt->execute(array($code,$desc));
   }
   catch(PDOException $e)
   {
--- server/php/getItem.php
+++ server/php/getItem.php
@@ -3,6 +3,6 @@
 function getTags($code){
   $tags = get_meta_tags('http://www.jumbo.com/zoeken?SearchTerm=' . $code);
   $desc = $tags['description'];
-  return $desc
+  return $desc;
 }
 ?>
