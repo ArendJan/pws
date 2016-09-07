ce837d517b51dd79918c68c9ea83659117615e40
VincentSLap <arendjan18@gmail.com>
2016-09-07 11:00:48 +0200

    newItem Conflict Fix (Hopelijk)

--- server/newItem.php
+++ server/newItem.php
@@ -2,40 +2,14 @@
 require_once("php/start.php");
 $conn = db();
 
-<<<<<<< HEAD
-  $code = $_GET['code'];
-  echo "Barcode: $code";
-=======
 $code = $_GET['code'];
 echo "Barcode: $code";
->>>>>>> origin/DatabaseEerste
 
 //Check of product al bestaat
 $checkstmt = $conn->prepare("SELECT barcode FROM producten WHERE barcode = :barcode");
 $checkstmt->bindParam(':barcode', $code);
 $checkstmt->execute();
 
-<<<<<<< HEAD
-      if($checkstmt->rowCount() > 0){
-          echo "Bestaat!";
-          //Doe +1 bij aantal van product
-          $upstmt = "UPDATE producten SET aantal = aantal + 1 WHERE barcode = ?";
-          $z = $conn->prepare($upstmt);
-          $z->execute(array($code));
-      } else {
-          echo "Bestaat niet!";
-          //Voeg product toe
-          try {
-            $addstmt = $conn->prepare("INSERT INTO producten (barcode, aantal) VALUES (?,1)");
-            $addstmt->execute(array($code));
-            }
-        catch(PDOException $e)
-            {
-            echo "n";
-            }
-            echo "y";
-      }
-=======
 if($checkstmt->rowCount() > 0){
   echo "Bestaat!";
   //Doe +1 bij aantal van product
@@ -46,8 +20,8 @@ if($checkstmt->rowCount() > 0){
   echo "Bestaat niet!";
   //Voeg product toe
   try {
-    $addstmt = $conn->prepare("INSERT INTO producten (barcode, aantal) VALUES (?,?)");
-    $addstmt->execute(array($code, 1));
+    $addstmt = $conn->prepare("INSERT INTO producten (barcode, aantal) VALUES (?,1)");
+    $addstmt->execute(array($code));
   }
   catch(PDOException $e)
   {
@@ -55,5 +29,4 @@ if($checkstmt->rowCount() > 0){
   }
   echo "y";
 }
->>>>>>> origin/DatabaseEerste
 ?>
