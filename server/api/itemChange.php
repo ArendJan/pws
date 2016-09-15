f1397a84b1cfd596584ae518368831eafc31ca9f
DjVinnii <vincentkling@msn.com>
2016-09-15 09:56:51 +0200

    delItem.php gemaakt

--- server/api/itemChange.php
+++ server/api/itemChange.php
@@ -1,3 +1,18 @@
 <?php
 
+require_once(../newItem.php);
+require_once(../delItem.php);
+require_once(../openItem.php);
+
+$code = $_POST["code"];
+$action = $_POST["action"];
+
+if($action == "add"){
+  add($code);
+} elseif ($action == "del") {
+  del($code);
+} elseif ($action == "open") {
+  upate($code);
+}
+
  ?>
