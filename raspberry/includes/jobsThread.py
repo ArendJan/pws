0fb7cafa2e8c97d5f9da0e795fb19dec8937dad1
Arend-Jan van Hilten <arendjan18@gmail.com>
2016-09-16 20:28:29 +0200

    github update & start van raspberry code

---
+++ raspberry/includes/jobsThread.py
@@ -0,0 +1,32 @@
+import os
+import settings
+import sys
+def start():
+    print "start jobs thread"
+    downloadGithub()
+
+
+def updateCode():
+    print "Downloading and installing new updates."
+    downloadGithub()
+    print "Restarting the program"
+    restart()
+def downloadGithub():
+    cmd ="curl -H \"Authorization: token 210928caef2212cda9586bb6dab335af19bfdf1a\" \-L https://api.github.com/repos/arendjan/pws/tarball > wut.tar.gz"
+    os.system(cmd)
+    cmd = "mkdir temp"
+    os.system(cmd)
+    cmd = "tar -xvzf wut.tar.gz -C temp"
+    os.system(cmd)
+    cmd = "mv -t ../pws temp/raspberry/*"
+def restart():
+    os.execl(sys.executable, sys.executable, *sys.argv)
+
+def jobsThread():
+    try:
+        url = "http://pws.svshizzle.com/status.php"
+        response = requests.get(url)
+        return json.loads(response.text)
+
+    except Interrupt:
+        print "ripppp"
---
+++ raspberry/includes/threads.py
@@ -0,0 +1,16 @@
+import jobsThread
+import keyboardThread
+def startAll():
+        keyboardThread.start()
+        jobsThread.start()
+
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    print "Don't run this file! :P"
