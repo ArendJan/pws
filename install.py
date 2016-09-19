5ea10e15f39a9cf8a27060ae4dacb328ade9cc3a
Arend-Jan van Hilten <arendjan18@gmail.com>
2016-09-19 20:19:19 +0200

    fix2

--- install.py
+++ install.py
@@ -1,5 +1,6 @@
 import os
 import sys
+print "anoesss"
 dir_path = os.path.dirname(os.path.realpath(__file__))
 print "this file installs the all the needed files and packages for the smart fridge"
 if os.getuid() != 0:
@@ -10,7 +11,7 @@ os.system("sudo apt-get install curl")
 cmd = "mkdir "+ dir_path + "/smartfridge"
 os.system(cmd)
 os.system("cd "+ dir_path + "/smartfridge")
-cmd ="curl -H \"Authorization: token 210928caef2212cda9586bb6dab335af19bfdf1a\" \-L https://api.github.com/repos/arendjan/pws/tarball > "+ dir_path + "/smartfridge/wut.tar.gz"
+cmd ="curl -H \"Authorization: token 210928caef2212cda9586bb6dab335af19bfdf1a\" \-L https://api.github.com/repos/arendjan/pws/tarball/AJMaandag199 > "+ dir_path + "/smartfridge/wut.tar.gz"
 os.system(cmd)
 cmd = "mkdir "+ dir_path + "/smartfridge/temp"
 os.system(cmd)
@@ -22,3 +23,4 @@ cmd = "rm -rf "+ dir_path + "/smartfridge/temp"
 os.system(cmd)
 cmd = "rm -rf "+ dir_path + "/smartfridge/wut.tar.gz"
 os.system(cmd)
+print "anoesss"
