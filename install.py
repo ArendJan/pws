f24009f8d63e5fdb777fe55ed29b652e0f115a04
Arend-Jan van Hilten <arendjan18@gmail.com>
2016-09-19 21:20:18 +0200

    install fix, userid command settings file

--- install.py
+++ install.py
@@ -1,26 +1,40 @@
 import os
 import sys
-print "anoesss"
+import argparse
+parser = argparse.ArgumentParser(description="Your personal userId")
+parser.add_argument('--userId')
+args = parser.parse_args()
+userid = args.userId
 dir_path = os.path.dirname(os.path.realpath(__file__))
 print "this file installs the all the needed files and packages for the smart fridge"
 if os.getuid() != 0:
     print "I need sudo right. plz (insert cute cat image)"
     sys.exit(1)
+print "updating packages list"
+os.system("sudo apt-get update")
 print "installing curl"
-os.system("sudo apt-get install curl")
+os.system("sudo apt-get -qq install curl")
+print "creating directory"
 cmd = "mkdir "+ dir_path + "/smartfridge"
 os.system(cmd)
-os.system("cd "+ dir_path + "/smartfridge")
+print "downloading code from python"
 cmd ="curl -H \"Authorization: token 210928caef2212cda9586bb6dab335af19bfdf1a\" -Ls https://api.github.com/repos/arendjan/pws/tarball/AJMaandag199 > "+ dir_path + "/smartfridge/wut.tar.gz"
 os.system(cmd)
-cmd = "mkdir "+ dir_path + "/smartfridge/temp"
+cmd = "mkdir -m 777 "+ dir_path + "/smartfridge/temp"
 os.system(cmd)
-cmd = "tar -xvzf "+ dir_path + "/smartfridge/wut.tar.gz -C "+ dir_path + "/smartfridge/temp"
+print "unzipping the file"
+cmd = "tar -xzf "+ dir_path + "/smartfridge/wut.tar.gz -C "+ dir_path + "/smartfridge/temp"
 os.system(cmd)
-cmd = "yes | cp -rfv "+ dir_path + "/smartfridge/temp/*/raspberry/* "+ dir_path + "/smartfridge "
+print "copying the files"
+cmd = "command cp -rf "+ dir_path + "/smartfridge/temp/*/raspberry/* "+ dir_path + "/smartfridge "
 os.system(cmd)
+print "removing garbage"
 cmd = "rm -rf "+ dir_path + "/smartfridge/temp"
 os.system(cmd)
 cmd = "rm -rf "+ dir_path + "/smartfridge/wut.tar.gz"
 os.system(cmd)
-print "anoesss"
+if(userid!=None):
+    f = open(dir_path+"/smartfridge/settings.py", 'w')
+    f.write("userId=\""+userid+"\"\ninterval=5\nurl=\"http://pws.svshizzle.com/api/\"");
+    print userid
+print "Done!"
