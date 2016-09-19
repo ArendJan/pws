265d6ff0b16df1b1d43939c32d633a1348d94e15
Arend-Jan van Hilten <arendjan18@gmail.com>
2016-09-19 20:08:26 +0200

    test with new way of files

--- install.py
+++ install.py
@@ -1,23 +1,24 @@
 import os
 import sys
+dir_path = os.path.dirname(os.path.realpath(__file__))
 print "this file installs the all the needed files and packages for the smart fridge"
 if os.getuid() != 0:
     print "I need sudo right. plz (insert cute cat image)"
     sys.exit(1)
 print "installing curl"
 os.system("sudo apt-get install curl")
-cmd = "mkdir ~/smartfridge"
+cmd = "mkdir "+ dir_path + "/smartfridge"
 os.system(cmd)
-os.system("cd ~/smartfridge")
-cmd ="curl -H \"Authorization: token 210928caef2212cda9586bb6dab335af19bfdf1a\" \-L https://api.github.com/repos/arendjan/pws/tarball > ~/smartfridge/wut.tar.gz"
+os.system("cd "+ dir_path + "/smartfridge")
+cmd ="curl -H \"Authorization: token 210928caef2212cda9586bb6dab335af19bfdf1a\" \-L https://api.github.com/repos/arendjan/pws/tarball > "+ dir_path + "/smartfridge/wut.tar.gz"
 os.system(cmd)
-cmd = "mkdir ~/smartfridge/temp"
+cmd = "mkdir "+ dir_path + "/smartfridge/temp"
 os.system(cmd)
-cmd = "tar -xvzf ~/smartfridge/wut.tar.gz -C ~/smartfridge/temp"
+cmd = "tar -xvzf "+ dir_path + "/smartfridge/wut.tar.gz -C "+ dir_path + "/smartfridge/temp"
 os.system(cmd)
-cmd = "mv -v ~/smartfridge/temp/*/raspberry/* ~/smartfridge "
+cmd = "mv -v "+ dir_path + "/smartfridge/temp/*/raspberry/* "+ dir_path + "/smartfridge "
 os.system(cmd)
-cmd = "rm -rf ~/smartfridge/temp"
+cmd = "rm -rf "+ dir_path + "/smartfridge/temp"
 os.system(cmd)
-cmd = "rm -rf ~/smartfridge/wut.tar.gz"
+cmd = "rm -rf "+ dir_path + "/smartfridge/wut.tar.gz"
 os.system(cmd)
