519e7e8cc398f27d200d92be1ee46ced564112a8
Arend-Jan van Hilten <arendjan18@gmail.com>
2016-09-21 17:07:48 +0200

    thread fix

--- server/api/itemChange.php
+++ server/api/itemChange.php
@@ -1,8 +1,8 @@
 <?php
 
-require_once("/include/newItem.php");
-require_once("/include/delItem.php");
-require_once("/include/openItem.php");
+require_once("include/newItem.php");
+require_once("include/delItem.php");
+require_once("include/openItem.php");
 
 $data = json_decode($_POST['JSON'],true);
 
