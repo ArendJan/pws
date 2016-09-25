efce64c7d1ef46048b58dcce80a41e0c80b13727
DjVinnii <vincentkling@msn.com>
2016-09-25 21:03:04 +0200

    getJobs van qrCode en text
    Met JSON echo

--- server/api/getJobs.php
+++ server/api/getJobs.php
@@ -1,6 +1,8 @@
 <?php
 
 include_once('../php/start.php');
+require_once("include/checkUserId.php");
+
 $conn = db();
 
 if (!isset($_POST['JSON'])){
@@ -13,4 +15,40 @@ $userId = $data['userId'];
 $status = $data["Status"];
 $type = $data["Type"];
 
+$json_array = array();
+
+if (checkUserId($userId) ==  true){
+  $stmt = $conn->prepare('SELECT * FROM jobs WHERE userId = ? AND type = ? AND status = ?');
+  $stmt->execute(array($userId, $type, $status));
+  $result = $stmt -> fetchAll();
+  foreach( $result as $row ) {
+    $type = $row['type'];
+    $status = $row['status'];
+
+    switch ($type) {
+      case "qrCode":
+        echo ":<br>Type: " . $row['type'] . "<br>Code: " . $row['qrcode'] . "<br>Status: " . $row['status'] . "<br>JobId: " . $row['ID'] . "<br><br>";
+        $row_array['Type'] = $row['type'];
+        $row_array['Code'] = $row['qrcode'];
+        $row_array['Status'] = $row['status'];
+        $row_array['JobId'] = $row['ID'];
+        array_push($json_array,$row_array);
+        break;
+      case "text":
+        echo ":<br>Type: " . $row['type'] . "<br>Text: " . $row['text'] . "<br>Status: " . $row['status'] . "<br>JobId: " . $row['ID'] . "<br><br>";
+        $row_array['Type'] = $row['type'];
+        $row_array['Text'] = $row['text'];
+        $row_array['Status'] = $row['status'];
+        $row_array['JobId'] = $row['ID'];
+        array_push($json_array,$row_array);
+        break;
+      default:
+        echo "Kaas";
+    }
+  }
+} else {
+  echo "You forgot your userId, or gave an invalid userId!";
+}
+
+echo json_encode($json_array);
 ?>
