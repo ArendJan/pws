55b20da31c0b575d9c6c0596d9f30ae886131f95
Arend-Jan van Hilten <arendjan18@gmail.com>
2016-09-28 19:52:07 +0200

    idk\

--- raspberry/eersteTests/keyboard1.py
+++ raspberry/eersteTests/keyboard1.py
@@ -4,7 +4,6 @@ import time
 def heardEnter():
     i,o,e = select.select([sys.stdin],[],[],0.0001)
     for s in i:
-        print s
         if s == sys.stdin:
             input = sys.stdin.readline()
             return True
