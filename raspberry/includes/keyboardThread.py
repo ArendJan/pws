ce1110ab0527713e290499fa71a8b55355cda67b
VincentSLap <arendjan18@gmail.com>
2016-10-10 16:14:17 +0200

    barcode scanner works, bit slow......

--- raspberry/includes/keyboardThread.py
+++ raspberry/includes/keyboardThread.py
@@ -1,9 +1,10 @@
 import RPi.GPIO as GPIO
 from threading import Thread
 import requests
-
+import json
 from .. import settings
 import time
+import sys
 knopIn = 9
 knopOpen = 22
 knopOut = 10
@@ -64,33 +65,35 @@ def buttonThread():
             statePrev = state
 
 def barcodeThread():
+    print "startBarcode"
     while True:
+        print "lelellelelellelel"
         data = scanCode() #Hier moet barcode code in komen
-        #TODO: CHeck!!!
-        print state
-        print data
-        request(data) #check of het een barcode is
+        print "lel"
+        thread = Thread(target=request, args=(data, state))
+        thread.start()
+        print "done"
 
-
-def request(code):
+def request(code, astate):
+    print code
+    print astate
     try:
         url = settings.url + "itemChange"
         postVars = json.dumps({
         "UserId":settings.userId,
         "Barcode":code,
-        "Action":add
+        "Action":astate
         })
         response = requests.post(url, data={"JSON":postVars})
         print response.text
 
-    except Requests.exceptions.RequestException as e:
+    except requests.exceptions.RequestException as e:
         print "ripppp"
         print # coding=utf-8
-        
+
 
 
 def scanCode():
-    import sys
 
     hid = { 4: 'a', 5: 'b', 6: 'c', 7: 'd', 8: 'e', 9: 'f', 10: 'g', 11: 'h', 12: 'i', 13: 'j', 14: 'k', 15: 'l', 16: 'm', 17: 'n', 18: 'o', 19: 'p', 20: 'q', 21: 'r', 22: 's', 23: 't', 24: 'u', 25: 'v', 26: 'w', 27: 'x', 28: 'y', 29: 'z', 30: '1', 31: '2', 32: '3', 33: '4', 34: '5', 35: '6', 36: '7', 37: '8', 38: '9', 39: '0', 44: ' ', 45: '-', 46: '=', 47: '[', 48: ']', 49: '\\', 51: ';' , 52: '\'', 53: '~', 54: ',', 55: '.', 56: '/'  }
 
@@ -105,7 +108,7 @@ def scanCode():
     done = False
 
     while not done:
-
+       #time.sleep(1)
        ## Get the character from the HID
        buffer = fp.read(8)
        for c in buffer:
