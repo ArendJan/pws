2ef95a65ab4df89dd5272fd24fad7305d36fb6e7
Arend-Jan van Hilten <arendjan18@gmail.com>
2016-10-23 19:42:14 +0200

    van alles voor verslag

--- install.py
+++ install.py
@@ -2,6 +2,7 @@ import os
 import sys
 import argparse
 import subprocess
+#Add some arguments to the command!!! Makes life easier!!!!
 parser = argparse.ArgumentParser(description="Your personal userId")
 parser.add_argument('--userId')
 parser.add_argument('--branch')
@@ -10,13 +11,17 @@ args = parser.parse_args()
 userid = args.userId
 branch = args.branch
 update = args.updateInstall
+
+#Get the path of the file that's currently running.
 dir_path = os.path.dirname(os.path.realpath(__file__))
+
+
 print "this file installs the all the needed files and packages for the smart fridge"
-if os.getuid() != 0:
+if os.getuid() != 0: #python voodoooo
     print "I need sudo right. plz (insert cute cat image)"
-    sys.exit(1)
+    sys.exit(1) # just die!
 print "updating packages list"
-if(update=="N"):
+if(update!="N"): #Then don't install updates (for updating and testing much faster, much wow)
     cmd = "sudo apt-get update"
     subprocess.Popen(cmd, shell=True).wait()
 
@@ -31,16 +36,23 @@ if(update=="N"):
     print "Updating pip 2/2"
     cmd = "sudo pip install --upgrade virtualenv"
     subprocess.Popen(cmd, shell=True).wait()
+
     print "installing pyBarcode"
     cmd = "sudo pip install pyBarcode"
     subprocess.Popen(cmd, shell=True).wait()
+
+
 print "creating directory"
 cmd = "mkdir "+ dir_path + "/smartfridge"
 subprocess.Popen(cmd, shell=True).wait()
+
+#use curl to download the whole github folder. For testing: add a branch to the command.
 print "downloading code from python"
 add = ""
+
 if(branch!=None):
     add = "/"+branch
+#The Authorization token is because it is a private repo.
 cmd ="curl -H \"Authorization: token 210928caef2212cda9586bb6dab335af19bfdf1a\" -Ls https://api.github.com/repos/arendjan/pws/tarball"+add+" > "+ dir_path + "/smartfridge/wut.tar.gz"
 subprocess.Popen(cmd, shell=True).wait()
 cmd = "mkdir -m 777 "+ dir_path + "/smartfridge/temp"
@@ -58,11 +70,15 @@ cmd = "rm -rf "+ dir_path + "/smartfridge/temp"
 subprocess.Popen(cmd, shell=True).wait()
 cmd = "rm -rf "+ dir_path + "/smartfridge/wut.tar.gz"
 subprocess.Popen(cmd, shell=True).wait()
+#Only sudo has permission, but we need to have permission!
 cmd = "sudo chmod -R 777 "+ dir_path + "/smartfridge/"
 subprocess.Popen(cmd, shell=True).wait()
+
+#If you've entered an userId, the whole settings file will be rewritten.
 if(userid!=None):
     f = open(dir_path+"/smartfridge/settings.py", 'w')
     f.write("userId=\""+userid+"\"\ninterval=5\nurl=\"http://pws.svshizzle.com/api/\"");
-    print userid
+
+#TODO:Create the Cronjob
 
 print "Done!"
--- raspberry/main.py
+++ raspberry/main.py
@@ -1,6 +1,10 @@
 from includes import *
 
 if __name__ == "__main__":
-    start()
+    print "This should be the appropriate way to start it, but not this time :P"
+    print "Underlaying code snippets use code from this folder, so you can't start it from the same folder #python4lyf."
+
+
+
 def start():
     threads.startAll()
