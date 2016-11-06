388d1aaff0c4f97bf014ab55abb46f6aad609b93
DjVinnii <vincentkling@msn.com>
2016-11-06 14:49:14 +0100

    Fixing bugs

--- server/api/include/openItem.php
+++ server/api/include/openItem.php
@@ -15,7 +15,7 @@ function openItem($code, $userId){
     try {
       $openstmt = $conn->prepare("UPDATE products SET open = open + 1 WHERE barcode = ? AND userId = ?");
       $openstmt->execute(array($code, $userId));
-      $open2stmt = $conn->prepare("UPDATE products SET closed = cloeds - 1 WHERE barcode = ? AND userId = ?");
+      $open2stmt = $conn->prepare("UPDATE products SET closed = closed - 1 WHERE barcode = ? AND userId = ?");
       $open2stmt->execute(array($code, $userId));
     }
     catch(PDOException $e)
