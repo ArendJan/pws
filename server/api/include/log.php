234d9a5265c7281580b25146c581321e223ebff9
VincentSLap <arendjan18@gmail.com>
2016-11-07 15:19:08 +0100

    log.php

--- server/api/include/log.php
+++ server/api/include/log.php
@@ -3,8 +3,9 @@
 include_once('../../php/start.php');
 $conn = db();
 
-function log(){
-
+function log($script, $params){
+  $logstmt = $conn->prepare("INSERT INTO log (time, script, params) VALUES (?,?,?)");
+  $logstmt->execute(array(date('Y-m-d H:i:s'),$script,$params));
 }
 
  ?>
