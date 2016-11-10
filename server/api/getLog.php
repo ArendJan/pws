c89bd33c995c87709bbf5ad0ce62cf38a97415d9
DjVinnii <vincentkling@msn.com>
2016-11-10 09:34:44 +0100

    getLog gemaakt

---
+++ server/api/getLog.php
@@ -0,0 +1,41 @@
+<?php
+
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+include_once('../php/start.php');
+require_once("include/checkUserId.php");
+
+$conn = db();
+
+if (!isset($_POST['JSON'])){
+  die("You have to post your values in _POST['JSON']");
+}
+
+$data = json_decode($_POST['JSON'],true);
+
+$userId = $data['UserId'];
+
+$json_array = array();
+
+if (checkUserId($userId) == false){
+  die ("You forgot your UserId, or gave an invalid UserId!");
+}
+
+  $stmt = $conn->prepare('SELECT * FROM logging WHERE userId = ?');
+  $stmt->execute(array($userId));
+
+$result = $stmt -> fetchAll();
+foreach( $result as $row ) {
+
+    $row_array['ID'] = $row['ID'];
+    $row_array['Time'] = $row['time'];
+    $row_array['Script'] = $row['script'];
+    $row_array['Params'] = $row['params'];
+    array_push($json_array,$row_array);
+
+}
+
+echo json_encode($json_array);
+?>
