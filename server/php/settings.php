033fa32a74a3a28dd181c14c5e894667c7e7851b
Arend-Jan van Hilten <arendjan18@gmail.com>
2016-12-07 19:28:45 +0100

    passwords away

--- server/php/settings.php
+++ server/php/settings.php
@@ -1,9 +1,9 @@
 <?php
-$servername = "rdbms.strato.de";
-$username = "U2672986";
-$password = "Koelkast1";
-$dbname = "DB2672986";
+$servername = "";
+$username = "";
+$password = "";
+$dbname = "";
 
-$masteruserId = "kaasblokje";
+$masteruserId = "";
 
 ?>
